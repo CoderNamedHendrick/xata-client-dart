cd1339bfe54827c47ccf19e5e0c4a273cf44a396
Hendrick <sebastinesoacatp@gmail.com>
2022-04-14 18:54:04 +0100

    feat: project init

---
+++ example/xata_client_dart_example.dart
@@ -0,0 +1,6 @@
+import 'package:xata_client_dart/xata_client_dart.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/xata_client_dart.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library xata_client_dart;
+
+export 'src/xata_client_dart_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
