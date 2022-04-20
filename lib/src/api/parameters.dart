28b9924bd6b29ae130786d3d641492fbd568dd05
Hendrick <sebastinesoacatp@gmail.com>
2022-04-20 01:55:30 +0100

    feat: parameter types

---
+++ lib/src/api/parameters.dart
@@ -0,0 +1,12 @@
+part of 'schemas.dart';
+
+typedef APIKeyNameParam = APIKeyName;
+typedef ColumnNameParam = ColumnName;
+typedef DBBranchNameParam = DBBranchName;
+typedef DBNameParam = DBName;
+typedef InviteIDParam = InviteID;
+typedef InviteKeyParam = InviteKey;
+typedef RecordIDParam = RecordID;
+typedef TableNameParam = TableName;
+typedef UserIDParam = UserID;
+typedef WorkspaceIDParam = WorkspaceID;
